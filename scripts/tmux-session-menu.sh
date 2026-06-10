62f62386dde9e1ecd62a666b5511b4c0bc8ea9ed
ar-rhc <11883576+ar-rhc@users.noreply.github.com>
2026-06-10 20:10:33 +1200

    cleanup: remove non-working dynamic session menu

--- scripts/tmux-session-menu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Dynamically generate a tmux menu of all active sessions
-# Based on the autodidactics blog post method.
-
-tmux list-sessions -F '#S' \
-| awk 'BEGIN {ORS=" "} {print $1, NR, "\"switch-client -t", $1 "\""}' \
-| xargs tmux display-menu -T "Switch session"
